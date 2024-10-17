2a6062bb0906ff9dbc8d39ac4b11dd3a93c81dcd
Albano <albigoal@outlook.com>
2024-10-17 19:15:22 +0200

    train data transfermarkt OFFLINE

--- main.R
+++ main.R
@@ -1,8 +1,21 @@
-# Load libraries
-library(tidyverse)
+# Set your working directory to the folder where the CSV files are located
+setwd("/Users/albanovukelaj/Desktop/Motherfucker/Football-Predictor-ML/dataset offline")
 
-# Sample code to read a dataset
-data <- read.csv("path/to/your/football_data.csv")
+# Load the CSV files
+transfers <- read.csv("transfers.csv")
+players <- read.csv("players.csv")
+player_valuations <- read.csv("player_valuations.csv")
+games <- read.csv("games.csv")
+game_lineups <- read.csv("game_lineups.csv")
+game_events <- read.csv("game_events.csv")
+competitions <- read.csv("competitions.csv")
+clubs <- read.csv("clubs.csv")
+club_games <- read.csv("club_games.csv")
+appearances <- read.csv("appearances.csv")
 
-# View the data
-head(data)
+# View the structure of one of the datasets (for example, games.csv)
+str(games)
+
+# You can also view the first few rows of any dataset
+head(games)
+
